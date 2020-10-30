c2ebf7073e105fcfa85916a0bfb5d14ee40bbd7e
Monicavila <di.monicavila@gmail.com>
2020-10-29 20:46:27 -0600

    pass test

--- index.js
+++ index.js
@@ -9,27 +9,36 @@ const userObj = {
 //============ Actividad ===============
 //Completa las siguientes funciones
 const getName = () => {
-    //1. Regresa como resultado el nombre del usuario    
+    //1. Regresa como resultado el nombre del usuario
+    return userObj.name;
+    
 }
 
 const getLastName = () => {
     //2. Regresa como resultado el apellido del usuario
+    return userObj.lastName;
 }
 
 const replaceEmail = (newEmail) => {
     //3. Remplaza el valor de la propiedad email de userObj y regresa como resultado todo el objeto
+    userObj.email = newEmail;
+    return userObj;
 }
 
 const getKeys = () => {
     //4. Regresa como resultado un arreglo de todas las propiedades que tiene el objeto userObj
+    return Object.keys(userObj);
 }
 
 const getValues = () => {
     //5. Regresa como resultado un arreglo con todos los valores del objeto userObj
+    return Object.values(userObj);
 }
 
 const addKeyValue = (key, value) => {
     //6. Agrega una llave y un valor al objeto userObj, respecto a los parametros key y value
+    userObj[key] = value;
+    return userObj;
 }
 
 //===== No modificar ====
